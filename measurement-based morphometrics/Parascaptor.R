3df11768f533b05988af94aab19c9f065d575765
anlong_MacBook <anlong_l@icloud.com>
2025-10-23 19:52:16 +0800

    feat: 添加形态测量分析脚本和Excel数据文件
    添加用于Euroscaptor和Parascaptor属的PCA和CVA分析R脚本
    添加包含测量数据的Excel文件para_log10.xlsx和euroscaptor_log10.xlsx
    添加物种形态测量分析结果的可视化图表

---
+++ measurement-based morphometrics/Parascaptor.R
@@ -0,0 +1,75 @@
+library(dplyr)
+library(ggplot2)
+library(readxl)
+library(Morpho)
+##PCA
+parascaptor <- read_excel("/hpcfile/home/xsn/Euroscaptor_PCA/para_log10.xlsx")
+          data <- parascaptor[, 2:16]
+          species_names <- parascaptor[[1]]
+           pca_result <- prcomp(data, scale. = TRUE)
+           pca_scores <- as.data.frame(pca_result$x)
+           pca_scores <- as.data.frame(pca_result$x)
+           pca_scores$species <- species_names
+           palette <- c('#636EFA', '#EF553B', '#00CC96')
+           pca_hull_data <- pca_scores %>%
+                     group_by(species) %>%
+                     do(hull = .[chull(.$PC1, .$PC2), ])
+           pca_hull_data <- bind_rows(pca_hull_data$hull)
+           pca_plot <- ggplot(pca_scores, aes(x = PC1, y = PC2, color = species)) +
+                     geom_point(size = 3) +
+                     geom_polygon(data = pca_hull_data, aes(x = PC1, y = PC2, group = species, fill = species), alpha = 0.2, color = NA) +
+                     scale_color_manual(values = palette) +
+                     scale_fill_manual(values = palette) +
+                    theme_minimal() +
+                    labs(title = expression("PCA of " * italic("Parascaptor") * " species - craniomandibular variables"),
+                                                     x = "Principal Component 1",
+                                                     y = "Principal Component 2")
+           pca_plot <- pca_plot + theme(
+                     plot.title = element_text(size = 22, hjust = 0.5),
+                     axis.title.x = element_text(size = 16),
+                     axis.title.y = element_text(size = 16),
+                     axis.text = element_text(size = 13),
+                     legend.text = element_text(size = 17)
+                )
+          print(pca_plot)
+ggsave("PCA_Parascaptor.pdf", plot = pca_plot, width = 12, height = 8)
+
+#CVA
+parascaptor <- read_excel("/hpcfile/home/xsn/Euroscaptor_PCA/para_log10.xlsx")
+       data <- parascaptor[, 2:16]
+       species_names <- parascaptor[[1]]
+       cva_result <- CVA(data, group = species_names)
+       cva_scores <- as.data.frame(cva_result$CVscores) 
+       cva_scores$species <- species_names
+       df <- data.frame(CV1 = cva_scores[, 1], CV2 = cva_scores[, 2], species = cva_scores$species)
+       palette <- c('#636EFA', '#EF553B', '#00CC96')
+       hull_data <- df %>%
+             group_by(species) %>%
+             do(hull = .[chull(.$CV1, .$CV2), ])
+       
+         # 将凸包数据转换为可绘制的格式
+         hull_data <- bind_rows(hull_data$hull)
+       
+         # 绘制CVA图，并添加凸包
+         cva_plot <- ggplot(df, aes(x = CV1, y = CV2, color = species)) +
+             geom_point(size = 3) +
+             geom_polygon(data = hull_data, aes(x = CV1, y = CV2, group = species, fill = species), 
+                                           alpha = 0.2, color = NA) +
+             scale_color_manual(values = palette) +
+             scale_fill_manual(values = palette) +
+             theme_minimal() +
+             labs(title = expression("CVA of " * italic("Parascaptor") * " species - craniomandibular variables"),
+                             x = "Canonical Variate 1",
+                             y = "Canonical Variate 2")
+       cva_plot <- cva_plot + theme(
+             plot.title = element_text(size = 22, hjust = 0.5),
+             axis.title.x = element_text(size = 16),
+             axis.title.y = element_text(size = 16),
+             axis.text = element_text(size = 13),
+             legend.text = element_text(size = 17)
+         )
+       # 输出绘图结果
+         print(cva_plot)
+       ggsave("CVA_Parascaptor.pdf", plot = cva_plot, width = 12, height = 8)
+
+         
